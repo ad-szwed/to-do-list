5cb0e2fa6df99a141d6e4014d4a323ad8ae24cce
ad-szwed <ad-szwed@users.noreply.github.com>
2021-01-29 16:02:26 +0000

    1.9 JQuery

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+function newItem(){
+    
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+
+  // input check and adding valid input
+  if (!inputValue) {
+    alert('no input');
+  } else {
+    li.append(inputValue);
+    $('#list').append(li);
+  }
+
+  // double-click crossing out
+  li.on("dblclick", function crossOut() {
+		li.toggleClass("strike");
+  });
+  
+  // delete button
+  let deleteButton = $('<crossOutButton></crossOutButton>');
+  deleteButton.append(document.createTextNode('X'));
+  li.append(deleteButton);
+  deleteButton.on('click', deleteListItem);
+
+  function deleteListItem() {
+      li.addClass('delete')
+  }
+}
+
+
